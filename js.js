d66f336744d514b11e8dc8804d0b6879402483d5
MonaQuimbamba <75567246+MonaQuimbamba@users.noreply.github.com>
2020-12-11 05:56:15 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,24 @@
+
+// modifier  un element  html
+
+let titre = document.querySelector('h1');
+titre.textContent = ' Hello word !'; 
+
+let par = document.querySelector('#p');
+par.textContent = 'rua';
+
+// lancer les notes 
+
+let note1 = document.querySelector('#cetu1');
+note1.textContent = ' 13';
+
+let note2 = document.querySelector('#cetu2');
+note2.textContent = '13';
+
+let note3 = document.querySelector('#cetu3');
+note3.textContent = '16';
+
+// calculer la moyenne
+
+
+
